f899e1bb82b1caa6e894c6770c2e3cd0a3c3d2e1
navyasri66 <115687653+navyasri66@users.noreply.github.com>
2024-01-09 22:06:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const app = express();
+const mongoose =require('mongoose')
+const cors = require('cors')
+
+app.get('/',(req,res)=>{res.send("hello i'm navya..")})
+
+app.use(cors())
+app.use(express.json())
+
+const Db = async()=>{
+    try {
+        await mongoose.connect('mongodb+srv://navya:Navya789@cluster0.0rv5sfb.mongodb.net/?retryWrites=true&w=majority').then(
+            ()=>{
+                console.log("Connected to DataBase...")
+            }
+        )
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+Db();
+
+app.listen(5000,()=>{console.log("Server is running on port 5000...")})
+
+
